3e7028afba8021ef74fce762479ef725b320d083
Mayur <mayur@Mayurs-MacBook-Air.local>
2017-12-07 12:43:58 +0530

    Updated with required changes

--- src/CredentialStoreItem.swift
+++ src/CredentialStoreItem.swift
@@ -5,30 +5,6 @@
 //  Created by Mayur on 03/10/17.
 //  Copyright © 2017 Odocon. All rights reserved.
 //
-/*
- MIT License
- 
- Copyright (c) 2017 Mayur Kore
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
-
 
 import Foundation
 
@@ -43,7 +19,7 @@ public enum CredentialStoreItemType: Int {
 open class CredentialStoreItem {
     var itemType: CredentialStoreItemType
     var itemTag: String
-    var itemData: Data
+    var itemData: Data?
     var isShared: Bool
     /**
      CreadentialStoreItem object to be stores inside CredentialStoreProvider.
@@ -54,7 +30,7 @@ open class CredentialStoreItem {
      - parameter itemData: Actual data for the item to be store inside CredentialStoreProvider/Keychain.
      - parameter isShared: Is this item shared under any shared access group.
      */
-    init(itemType: CredentialStoreItemType, itemTag: String, itemData: Data, isShared: Bool) {
+    public init(itemType: CredentialStoreItemType, itemTag: String, itemData: Data?, isShared: Bool) {
         self.itemType = itemType
         self.itemTag = itemTag
         self.itemData = itemData
